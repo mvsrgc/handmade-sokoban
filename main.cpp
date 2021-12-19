9701193c4636970bb59ec2283bf2012e8ae5eafb
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-12-19 15:40:03 -0500

    feat: render weird gradient

--- main.cpp
+++ main.cpp
@@ -1,15 +1,44 @@
 #include <iostream>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
+#include <sys/mman.h>
 
 static bool Running;
 static SDL_Texture *Texture;
-static void *Pixels;
-static int TextureWidth;
+static void *BitmapMemory;
+static int BitmapWidth;
+static int BitmapHeight;
+static int BytesPerPixel = 4;
+
+static void RenderWeirdGradient(int XOffset, int YOffset) {
+    int Pitch = BitmapWidth * BytesPerPixel;
+    uint8_t *Row = (uint8_t *) BitmapMemory;
+
+    for (int Y = 0; Y < BitmapHeight; ++Y) {
+        uint32_t *Pixel = (uint32_t *) Row;
+
+        for (int X = 0; X < BitmapWidth; ++X) {
+            *Pixel = (uint8_t) (X + XOffset);
+            ++Pixel;
+
+            *Pixel = (uint8_t) (Y + YOffset);
+            ++Pixel;
+
+            *Pixel = 0;
+            ++Pixel;
+
+            *Pixel = 0;
+            ++Pixel;
+        }
+
+        Row += Pitch;
+    }
+
+}
 
 static void SDLResizeTexture(SDL_Renderer *Renderer, int Width, int Height) {
-    if (Pixels) {
-        free(Pixels);
+    if (BitmapMemory) {
+        munmap(BitmapMemory, BitmapWidth * BitmapHeight * BytesPerPixel);
     }
     if (Texture) {
         SDL_DestroyTexture(Texture);
@@ -19,12 +48,13 @@ static void SDLResizeTexture(SDL_Renderer *Renderer, int Width, int Height) {
                                 SDL_TEXTUREACCESS_STREAMING,
                                 Width,
                                 Height);
-    TextureWidth = Width;
-    Pixels = malloc(Width * Height * 4);
+    BitmapWidth = Width;
+    BitmapHeight = Height;
+    BitmapMemory = mmap(0, Width * Height * BytesPerPixel, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 }
 
 static void SDLUpdateWindow(SDL_Window *Window, SDL_Renderer *Renderer) {
-    SDL_UpdateTexture(Texture, 0, Pixels, TextureWidth * 4);
+    SDL_UpdateTexture(Texture, 0, BitmapMemory, BitmapWidth * BytesPerPixel);
 
     SDL_RenderCopy(Renderer, Texture, 0, 0);
 
@@ -39,7 +69,7 @@ void HandleEvent(SDL_Event *Event) {
             break;
         case SDL_WINDOWEVENT: {
             switch (Event->window.event) {
-                case SDL_WINDOWEVENT_RESIZED: {
+                case SDL_WINDOWEVENT_SIZE_CHANGED: {
                     SDL_Window *Window = SDL_GetWindowFromID(Event->window.windowID);
                     SDL_Renderer *Renderer = SDL_GetRenderer(Window);
                     printf("SDL_WINDOWEVENT_RESIZED (%d, %d)\n", Event->window.data1, Event->window.data2);
@@ -75,15 +105,22 @@ int main() {
 
     SDL_Renderer *Renderer = SDL_CreateRenderer(Window, -1, 0);
 
-    SDL_Texture *Texture = SDL_CreateTexture(Renderer, SDL_PIXELFORMAT_ABGR8888,
-                                             SDL_TEXTUREACCESS_STREAMING, Width,
-                                             Height);
+    SDL_GetWindowSize(Window, &Width, &Height);
+    SDLResizeTexture(Renderer, Width, Height);
 
     Running = true;
+    int XOffset = 0;
+    int YOffset = 0;
     while (Running) {
         SDL_Event Event;
         SDL_WaitEvent(&Event);
-        HandleEvent(&Event);
+        while (SDL_PollEvent(&Event)) {
+            HandleEvent(&Event);
+        }
+        RenderWeirdGradient(XOffset, YOffset);
+        SDLUpdateWindow(Window, Renderer);
+        ++XOffset;
+        YOffset += 2;
     }
 
     SDL_Quit();
